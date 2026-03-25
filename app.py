e8bfd9e5eec09acbd921fb814e9c44af7646b12b
sonali <kommawarsonalisonu@gmail.com>
2026-03-25 14:32:22 +0530

    Resolved merge conflict between feature-login and main

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+tasks = []
+
 @app.route("/")
 def home():
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/add", methods=["POST"])
+def add_task():
+    task = request.form.get("task")
+    tasks.append(task)
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/status")
+def status():
     return "Git Workflow Automation Running!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
