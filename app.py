eccbddcb58c4645283279fa4cb7370a5339fe616
sonali <kommawarsonalisonu@gmail.com>
2026-03-25 14:57:33 +0530

    Team task updated

--- app.py
+++ app.py
@@ -11,12 +11,9 @@ def home():
 @app.route("/add", methods=["POST"])
 def add_task():
     task = request.form.get("task")
-    tasks.append(task)
+    if task:
+        tasks.append(task)
     return render_template("index.html", tasks=tasks)
-
-@app.route("/status")
-def status():
-    return "Git Workflow Automation Running!"
 
 if __name__ == "__main__":
     app.run(debug=True)
